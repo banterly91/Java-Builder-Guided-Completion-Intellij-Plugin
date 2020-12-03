d9ab407bdb6579d14544050a8fcc3bc7d1492001
dragosb <barosan.dragos@gmail.com>
2020-12-03 23:12:29 +0100

    Add test class. No tests for now

---
+++ src/test/java/net/banterly/buildercompletion/BuilderCompletionProvider.java
@@ -0,0 +1,9 @@
+package net.banterly.buildercompletion;
+
+import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixture4TestCase;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BuilderCompletionProvider extends LightPlatformCodeInsightFixture4TestCase {
+
+}
