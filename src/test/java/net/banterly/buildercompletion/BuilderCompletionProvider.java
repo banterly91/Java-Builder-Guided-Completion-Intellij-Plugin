16cca922809eb002d8d971e2a730080260d1e3bf
dragosb <barosan.dragos@gmail.com>
2020-12-05 23:14:16 +0100

    remove unused classes

--- src/test/java/net/banterly/buildercompletion/BuilderCompletionProvider.java
+++
@@ -1,9 +0,0 @@
-package net.banterly.buildercompletion;
-
-import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixture4TestCase;
-import org.junit.Assert;
-import org.junit.Test;
-
-public class BuilderCompletionProvider extends LightPlatformCodeInsightFixture4TestCase {
-
-}
